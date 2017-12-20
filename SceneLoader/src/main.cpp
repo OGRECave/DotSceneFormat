9ea1582801728c169ae36eee5073681dc30d3a58
Pavel Rojtberg <rojtberg@gmail.com>
2017-12-21 00:57:43 +0100

    example: update to latest OgreBites changes (Ogre 1.10.11)

--- SceneLoader/src/main.cpp
+++ SceneLoader/src/main.cpp
@@ -9,12 +9,12 @@ class SceneLoadSample : public OgreBites::ApplicationContext, public OgreBites::
 public:
     SceneLoadSample();
     void setup();
+    void locateResources();
     bool keyPressed(const OgreBites::KeyboardEvent& evt);
 };
 
 SceneLoadSample::SceneLoadSample() : OgreBites::ApplicationContext("SceneLoadSample")
 {
-    addInputListener(this);
 }
 
 bool SceneLoadSample::keyPressed(const OgreBites::KeyboardEvent& evt)
@@ -26,10 +26,18 @@ bool SceneLoadSample::keyPressed(const OgreBites::KeyboardEvent& evt)
     return true;
 }
 
+void SceneLoadSample::locateResources()
+{
+    OgreBites::ApplicationContext::locateResources();
+    Ogre::ResourceGroupManager::getSingleton().addResourceLocation(".", "FileSystem", "Scene");
+}
+
 void SceneLoadSample::setup(void)
 {
     // do not forget to call the base first
     OgreBites::ApplicationContext::setup();
+
+    addInputListener(this);
 
     // get a pointer to the already created root
     Ogre::Root* root = getRoot();
@@ -40,13 +48,14 @@ void SceneLoadSample::setup(void)
     shadergen->addSceneManager(scnMgr);
 
     DotSceneLoader loader;
-    loader.parseDotScene("example.scene", "General", scnMgr);
+    loader.parseDotScene("example.scene", "Scene", scnMgr);
 
     // create the camera
     Ogre::Camera* cam = scnMgr->getCameras().begin()->second;
     cam->setAutoAspectRatio(true);
 
     OgreBites::CameraMan* camman = new OgreBites::CameraMan(cam->getParentSceneNode());
+    camman->setStyle(OgreBites::CS_ORBIT);
     addInputListener(camman);
 
     // and tell it to render into the main window
